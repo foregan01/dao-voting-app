5beb77b8a1069697ca2d758f5a7c96ab7767aa97
Elegbe Osiforegan <35657877+foregan01@users.noreply.github.com>
2022-09-14 12:23:46 +0000

    OGM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import { ApeDaoProvider } from '../context/context'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mumbai
